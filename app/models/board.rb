3384fa4c47127bb9f1ed7fa7a2aa5ae7a58c0cc4
tatsuo <user@usernoMacBook-Pro.local>
2024-09-17 12:01:03 +0900

    神泡掲示板の画像アップロード機能

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,4 +3,6 @@ class Board < ApplicationRecord
   validates :body, presence: true, length: { maximum: 65_535 }
 
   belongs_to :user
+
+  mount_uploader :board_image, BoardImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_15_101245) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_17_022359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_15_101245) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
