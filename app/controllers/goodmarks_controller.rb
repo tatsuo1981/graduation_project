8442661f1b2af34fcd1643546fc91013d477e56b
tatsuo <user@usernoMacBook-Pro.local>
2024-09-18 17:53:43 +0900

    いいねボタンのajax化

--- app/controllers/goodmarks_controller.rb
+++ app/controllers/goodmarks_controller.rb
@@ -1,13 +1,11 @@
 class GoodmarksController < ApplicationController
   def create
-    board = Board.find(params[:board_id])
-    current_user.goodmark(board)
-    redirect_to boards_path, success: t('.success')
+    @board = Board.find(params[:board_id])
+    current_user.goodmark(@board)
   end
 
   def destroy
-    board = current_user.goodmarks.find(params[:id]).board
-    current_user.ungoodmark(board)
-    redirect_to boards_path, success: t('.success'), status: :see_other
+    @board = current_user.goodmarks.find(params[:id]).board
+    current_user.ungoodmark(@board)
   end
 end
