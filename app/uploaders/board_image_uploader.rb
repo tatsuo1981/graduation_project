85049106b9c2e1730e9fc37979a42250818efc51
tatsuo <user@usernoMacBook-Pro.local>
2024-10-03 10:34:20 +0900

    S3外部ストレージ

--- app/uploaders/board_image_uploader.rb
+++ app/uploaders/board_image_uploader.rb
@@ -6,10 +6,10 @@ class BoardImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
-  if Rails.env.development? || Rails.env.test? 
-    storage :file
-  else
+  if Rails.env.production?
     storage :fog
+  else
+    storage :file
   end
 
   # Override the directory where uploaded files will be stored.
