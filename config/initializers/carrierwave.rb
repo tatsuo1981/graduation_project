f5d2a4973694628e0eddd15fca6b4c023b540e7e
tatsuo <user@usernoMacBook-Pro.local>
2024-10-04 09:15:45 +0900

    Update carrierwave configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,13 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
     config.storage :fog
-    config.fog_directory  = 't.style'
+    config.fog_directory  = 't-style'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1',
       path_style: true
     }
 end
+
