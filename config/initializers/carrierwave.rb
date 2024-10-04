335fd88861a7ec8afcfb2fbdfdecfbd06bcf61b2
tatsuo <user@usernoMacBook-Pro.local>
2024-10-05 01:14:13 +0900

    変更内容の説明

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     config.fog_directory  = 't-style'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1',
       path_style: true
