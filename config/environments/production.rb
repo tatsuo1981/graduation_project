9fd5b4afbd8226e4afb91e2614958e0242b6a7f3
tatsuo <user@usernoMacBook-Pro.local>
2024-10-06 17:52:40 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'https://graduation-project-p5up.onrender.com',
+    domain:               'graduation-project-p5up.onrender.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
